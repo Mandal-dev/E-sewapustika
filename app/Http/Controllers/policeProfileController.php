1fa4978ef722188694e3bda2b4f3b0fb2ec26acb
Mandal-dev <mandalpooja789@gmail.com>
2025-09-22 18:53:12 +0530

    refactor: Simplify warning message for unauthorized access in policeProfileController

--- app/Http/Controllers/policeProfileController.php
+++ app/Http/Controllers/policeProfileController.php
@@ -18,7 +18,7 @@ class policeProfileController extends Controller
             $user = Session::get('user');
 
             if (!$user) {
-                Log::warning('Access denied: user not logged in, plz login first...');
+                Log::warning('Access denied: user not logged in...');
                 return redirect('/')->with('error', 'Please login first.');
             }
 
